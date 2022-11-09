dbaac184a2efad09afff418c20d843fef23fbe86
Ldragk <lucasvm.ti@gmail.com>
2022-11-09 17:50:12 -0300

    att: Excluir

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 <div align="center">
 
-# PROGRAMA PARA BANCO DE DADOS E CONSULTA DE ITENS EM ESTOQUE DE LOJA
+# PROGRAMA PARA ESTOQUE DE LOJAS
 
 ## Python
 
 <p>Programinha desenvolvido nas aulas do Curso de Python da Fiap</p>
-<p>Cadastre os equipamentos e as suas especificações e depois consulte quando quiser!</p>
+<p>Cadastre os equipamentos e as suas especificações, é possível consultar e exluir itens quando quiser!</p>
 
 <br>
 
-<p>Saída:</p>
+## Saída:
 <br>
+<p>Buscando item</p>
 <img width="70%" src="./public/print.png" alt="print saída">
+<br><br>
+<p>Excluindo item</p>
+<img width="70%" src="./public/print2.png" alt="print saída">
 
 </div>
--- main.py
+++ main.py
@@ -20,11 +20,29 @@ for indice in range(0, len(equipamentos)):
     print("Serial.......: ", seriais[indice])
     print("Departamento.: ", departamentos[indice])
 
-busca = input("Digite o nome do equipamento que deseja buscar: ").upper()
+print("1 - Para buscar um item. \
+      \n2 - Para excluir um item.")
 
-for indice in range(0, len(equipamentos)):
-    if busca == equipamentos[indice]:
-        print("Valor..: ", valores[indice])
-        print("Serial.: ", seriais[indice])
+opcoes = input("O que deseja fazer? ")
+
+if opcoes == "1":
+    busca = input("Digite o nome do equipamento que deseja buscar: ").upper()
+    for indice in range(0, len(equipamentos)):
+        if busca == equipamentos[indice]:
+            print("Valor..: ", valores[indice])
+            print("Serial.: ", seriais[indice])
+
+elif opcoes == "2":
+    excluir = input(
+        "Digite o nome do equipamento que deseja excluir: ").upper()
+    for indice in range(0, len(equipamentos)):
+        if excluir == equipamentos[indice]:
+           del equipamentos[indice]
+           del valores[indice]
+           del seriais[indice]
+           del departamentos[indice]
+           print("Item exluido com sucesso!")
     
 
+else:
+    print("Opção invalida!")
